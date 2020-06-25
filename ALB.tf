1e104eca54e37c87247ab7e9cd2838abcc71be99
kainat khalid <kainat.khalid@northbaysolutions.net>
2020-06-25 13:55:24 +0500

    Added ALB ,ASG launch template & lambda policies

--- ALB.tf
+++ ALB.tf
@@ -2,8 +2,8 @@ resource "aws_lb" "test" {
   name               = "test-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.sg-alb.id]
-  subnets            = [aws_subnet.prod-subnet-public-1.id]
+  security_groups    = ["${aws_security_group.sg-alb.id}"]
+  subnets            = ["${aws_subnet.prod-subnet-public-1.id}"]
 
   enable_deletion_protection = true
 
